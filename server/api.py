dd8dd847f8512faddca8b7ac3c407b03ec1983e2
esc <esc@esc-qt>
2020-06-01 14:04:12 -0500

    endpoints done

--- server/api.py
+++ server/api.py
@@ -6,5 +6,19 @@ api = Blueprint("api", __name__)
 
 @api.route("/market_summary")
 def market_summary():
-    test_req = requests.get("https://api.coinlore.net/api/global/")
-    return test_req.text
+    r = requests.get("https://api.coinlore.net/api/global/")
+    return r.text
+
+
+@api.route("/top_summary/<start>/<limit>")
+def top_summary(start, limit):
+    r = requests.get(
+        "https://api.coinlore.net/api/tickers/?start=" + start + "&limit=" + limit
+    )
+    return r.text
+
+
+@api.route("/summary/<id_from_ticker>")
+def single_ticker(id_from_ticker):
+    r = requests.get("https://api.coinlore.net/api/ticker/?id=" + id_from_ticker)
+    return r.text
